94d9b78c61e18d5bc5135ee5b86fca086944e9e7
Klea Korbi <kleakorbi@gmail.com>
2020-05-03 19:08:25 +0200

    Added user age

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
+static_data = [{'name':'Genci', 'email':'genci@mail.com'},
+               {'name':'Mondi', 'email':'mondi@mail.com'},
+               {'name':'Berti', 'email':'berti@mail.com'}
+]
+
 @app.route('/')
 @app.route('/home')
 def index():
@@ -11,6 +16,10 @@ def index():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/users')
+def users():
+	return render_template('users.html', data=static_data)
  
 if __name__ == '__main__':
 	app.run(debug=True)
