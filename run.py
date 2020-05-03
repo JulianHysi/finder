28aba3670a08e9d7ff70d1c846cc61e9867d21de
Julian Hysi <julian_hysi@yahoo.com>
2020-05-03 17:49:09 +0200

    Added about route and debug mode

--- run.py
+++ run.py
@@ -2,6 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-	return 'Hello Web'
+	return '<h2>Hello Web</h2>'
+
+@app.route('/about')
+def about():
+	return '<center>This is the about page</center>'
  
+if __name__ == '__main__':
+	app.run(debug=True)
