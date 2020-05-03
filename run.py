864d4d9082ce02cf75dc06e7cec71a4531f131e4
1tsr101 <carciu.94anxhela@outlook.com>
2020-05-03 18:17:18 +0200

    Added about.html and index.html

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def index():
-	return '<h2>Hello Web</h2>'
+	return render_template('index.html')
 
 @app.route('/about')
 def about():
-	return '<center>This is the about page</center>'
+	return render_template('about.html')
  
 if __name__ == '__main__':
 	app.run(debug=True)
