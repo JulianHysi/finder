c53d34f34830b7bc13ea33ed53fba88476bf7478
Julian Hysi <julian_hysi@yahoo.com>
2020-05-24 19:24:42 +0200

    Made the sign up work with the db

--- finder/__init__.py
+++ finder/__init__.py
@@ -1,6 +1,7 @@
 from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 import finder.routes
--- finder/routes.py
+++ finder/routes.py
@@ -4,6 +4,8 @@ from flask import redirect
 from flask import url_for
 from finder import app
 from finder.forms import SignUpForm, LogInForm
+from finder import db, bcrypt
+from finder.models import User
 
 
 static_data = [{'name':'Genci', 'email':'genci@mail.com'},
@@ -29,7 +31,14 @@ def users():
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
-        flash(f'The account {form.username.data} was created successfully', 'success')
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username, email=email, password=hashed_pwd)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'The account {username} was created successfully', 'success')
         return redirect(url_for('index'))
     return render_template('signup.html', form=form)
 
