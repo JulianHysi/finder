05836f8dfb0789bf3dbdb9c402ad961a44e79e3c
Julian Hysi <julian_hysi@yahoo.com>
2020-05-24 18:16:22 +0200

    Added models.py and created the db

--- finder/__init__.py
+++ finder/__init__.py
@@ -1,9 +1,13 @@
 from os import environ
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+
+db = SQLAlchemy(app)
 
 import finder.routes
---
+++ finder/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime 
+from finder import db
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(15), unique=True, nullable=False)
+	email = db.Column(db.String(60), unique=True, nullable=False)
+	password = db.Column(db.String(120), nullable=False)
+	date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	profile = db.relationship('Profile', backref='parent', uselist=False)
+
+	def __repr__(self):
+		return f'Username: {self.username}, Email: {self.email}\n'
+
+
+class Profile(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	full_name = db.Column(db.String(60))
+	nick_name = db.Column(db.String(60))
+	email = db.Column(db.String(60))
+	phone_number = db.Column(db.String(30))
+	address = db.Column(db.String(60))
+	profile_pic = db.Column(db.String(60))
+	birth_date = db.Column(db.DateTime)
+	birth_place = db.Column(db.String(60))
+	website = db.Column(db.String(60))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+
+	def __repr__(self):
+		return f'Full Name: {self.full_name}, Phone: {self.phone_number}\n'
