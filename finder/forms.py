c45f61ee727627650c24f94293ad0af4d06d41b4
Julian Hysi <julian_hysi@yahoo.com>
2020-05-30 16:55:51 +0200

    Validated for unique username & email

--- finder/forms.py
+++ finder/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from finder.models import User
 
 
 class SignUpForm(FlaskForm):
@@ -12,6 +13,16 @@ class SignUpForm(FlaskForm):
     confirm_password = PasswordField('Confirm password', 
                                      validators=[EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken!')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already taken!')
 
 
 class LogInForm(FlaskForm):
